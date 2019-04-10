182416454f4e6b51ada0f61c33d078dec9e246a0
Donald Yeh <Donald.Yeh@gmail.com>
2019-04-10 09:32:37 -0400

    adds home controller and view

--- seed.sql
+++ seed.sql
@@ -6,30 +6,31 @@ values
     ('Potato Head', 'iH8myLife@spud.gov', '$2a$10$Eg2fJeOgL2JV6xwL8cL45O9M8I3BvQYTopFWfv5jGjPfkw/AAv4tG')
 ;
 
-insert into methods 
-(language, method, description, snippet)
+insert into methods
+    (language, method, description, snippet)
 values
-('JavaScript', 'arr.forEach()', 
-'.forEach() requires an array, and will call a provided function for each value in that array. 
+    ('JavaScript', 'arr.forEach()',
+        '.forEach() requires an array, and will call a provided function for each value in that array. 
 function(currentValue, index, arr)
 It is required to pass this function the currentValue, the other two paramaters are optional.',
-'const arr = [1, 2, 3, 4, 5]
+        'const arr = [1, 2, 3, 4, 5]
 arr.forEach(function (numb){
     console.log(numb)
 })')
 ;
 
 insert into articles
-(method_id, article)
+    (method_id, article)
 VALUES
-(1,'https://www.w3schools.com/jsref/jsref_foreach.asp'),
-(1, 'JavaScript','https://coderwall.com/p/kvzbpa/don-t-use-array-foreach-use-for-instead')
+    (1, 'https://www.w3schools.com/jsref/jsref_foreach.asp'),
+    (1, 'https://coderwall.com/p/kvzbpa/don-t-use-array-foreach-use-for-instead')
 ;
 
 insert into videos
-(link, method_id)
+    (link, method_id)
+ß
 VALUES
-('https://www.youtube.com/watch?v=159EAISAxwg', 1)
+    ('https://www.youtube.com/watch?v=159EAISAxwg', 1)
 ;
 -- Banana = BananaPeel
 -- Stevie = CoolGuy420
