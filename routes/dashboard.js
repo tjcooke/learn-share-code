7959845cb9ae270346d86a539a1cf04b2eeebf72
Benjamin Hallquist <benhall84707@gmail.com>
2019-04-11 10:10:09 -0400

    added views

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-const dashRoute = require('express').Router();
-const dashPage = require('../controllers/home');
-
-dashRoute.get('/', dashPage);
-
-module.exports = dashRoute;
-=======
 const dashboardRoute = require('express').Router();
 const {dashboardPage, dashboardPost} = require('../controllers/dashboard');
 
@@ -14,5 +6,3 @@ dashboardRoute.get('/', dashboardPage)
 dashboardRoute.post('/',dashboardPost)
 
 module.exports = dashboardRoute;
-
->>>>>>> 99393bd7b228ce15f7767e22a57552197e668db0
