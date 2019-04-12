71b680814b056c4f45d7e81d0d77b82ee824b56a
Benjamin Hallquist <benhall84707@gmail.com>
2019-04-12 09:07:15 -0400

    styled language homepage

--- controllers/home.js
+++ controllers/home.js
@@ -2,16 +2,14 @@ const axios = require('axios');
 
 
 async function homePage(req, res) {
-    const theNews = await axios.get('https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=58d03d1f371d4f4f84963a2e95de2163')
-    console.log('44444444444444444444')
-    console.log(theNews.data.articles)
-    let myNews = theNews.data.articles.filter((eaArticle)=>{
+    let theNews = await axios.get('https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=58d03d1f371d4f4f84963a2e95de2163')
+    theNews = theNews.data.articles.filter((eaArticle)=>{
         return (eaArticle.urlToImage)
     })
     res.render('home', {
         locals: {
             email: '',
-            news:myNews,
+            news:theNews,
             message: "you're on the homepage"
         }
     });
